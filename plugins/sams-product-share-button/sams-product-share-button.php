92119210a6cb231997997373fa4200daf07226a9
samzih <samzih1337@gmail.com>
2023-04-19 22:13:36 +0200

    created plugin file & added its header comments

---
+++ plugins/sams-product-share-button/sams-product-share-button.php
@@ -0,0 +1,8 @@
+<?php
+
+/*
+ * Plugin Name: Sam's Product Share Button
+ * Description: Simple plugin for displaying a share button on a single product page under the product metadata (requires WooCommerce to be installed).
+ * Version: 1.0
+ * Author: Sam Azimi
+ */
