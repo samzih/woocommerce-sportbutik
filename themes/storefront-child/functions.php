d5e6ef5869fb8e6ab6b069bf40336b827d3b75ce
samzih <samzih1337@gmail.com>
2023-04-20 19:17:08 +0200

    function for removing related products section

--- themes/storefront-child/functions.php
+++ themes/storefront-child/functions.php
@@ -7,3 +7,11 @@ function add_custom_checkout_content()
     echo '<p>Behöver du hjälp? Ring oss på <a href="tel:' . $phone_number . '">' . $phone_number . '</a></p>';
 }
 add_action('woocommerce_review_order_after_submit', 'add_custom_checkout_content', 10);
+
+
+// removes the related products section on single product pages
+function remove_related_products()
+{
+    remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20);
+}
+add_action('wp', 'remove_related_products');
